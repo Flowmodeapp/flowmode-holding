fd0711ec635efef318ecac4376eeb3f9d3288245
Flowmodeapp <murphysmate420@gmail.com>
2025-07-04 16:08:22 +0100

    Fix: Convert sync script to async for Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -62,11 +62,11 @@ const App = () => {
       */}
 
       {/* Slogan at the bottom center */}
-      <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2 text-center z-10 px-4"> {/* Adjusted bottom position */}
+      <div className="absolute bottom-15 left-1/2 transform -translate-x-1/2 text-center z-10 px-4"> {/* Adjusted bottom position */}
         <p className="text-xl md:text-2xl leading-relaxed text-white">
           Escape the noise.<br />
           <span className="font-bold">Enter Flowmode.</span><br />
-          <span >A guide is coming...</span>
+          <span >A guide is coming.</span>
         </p>
       </div>
     </main>
